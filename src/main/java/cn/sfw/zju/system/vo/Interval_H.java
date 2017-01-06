ae0667a6a98c8696bfa031c6a50e4f6c521e5e21
zjulyp <neulyp@outlook.com>
2017-01-06 21:10:49 +0800

    hour bean

---
+++ src/main/java/cn/sfw/zju/system/vo/Interval_H.java
@@ -0,0 +1,125 @@
+package cn.sfw.zju.system.vo;
+
+/**
+ * 以日为间隔单位，统计获得的中间表
+ * @author neuly
+ *
+ */
+public class Interval_H {
+	private Integer cst_id;
+	private String cst_name;
+	private Long mdi_ts;
+	private String cdate;
+	
+	private Double H0;
+	private Double H1;
+	private Double H2;
+	
+	private Double H10;
+	private Double H11;
+	private Double H12;
+	
+	private Double H20;
+	private Double H21;
+	private Double H22;
+	
+	private Double H70;
+	private Double H71;
+	private Double H72;
+	public Integer getCst_id() {
+		return cst_id;
+	}
+	public void setCst_id(Integer cst_id) {
+		this.cst_id = cst_id;
+	}
+	public String getCst_name() {
+		return cst_name;
+	}
+	public void setCst_name(String cst_name) {
+		this.cst_name = cst_name;
+	}
+	public Long getMdi_ts() {
+		return mdi_ts;
+	}
+	public void setMdi_ts(Long mdi_ts) {
+		this.mdi_ts = mdi_ts;
+	}
+	public String getCdate() {
+		return cdate;
+	}
+	public void setCdate(String cdate) {
+		this.cdate = cdate;
+	}
+	public Double getH0() {
+		return H0;
+	}
+	public void setH0(Double h0) {
+		H0 = h0;
+	}
+	public Double getH1() {
+		return H1;
+	}
+	public void setH1(Double h1) {
+		H1 = h1;
+	}
+	public Double getH2() {
+		return H2;
+	}
+	public void setH2(Double h2) {
+		H2 = h2;
+	}
+	public Double getH10() {
+		return H10;
+	}
+	public void setH10(Double h10) {
+		H10 = h10;
+	}
+	public Double getH11() {
+		return H11;
+	}
+	public void setH11(Double h11) {
+		H11 = h11;
+	}
+	public Double getH12() {
+		return H12;
+	}
+	public void setH12(Double h12) {
+		H12 = h12;
+	}
+	public Double getH20() {
+		return H20;
+	}
+	public void setH20(Double h20) {
+		H20 = h20;
+	}
+	public Double getH21() {
+		return H21;
+	}
+	public void setH21(Double h21) {
+		H21 = h21;
+	}
+	public Double getH22() {
+		return H22;
+	}
+	public void setH22(Double h22) {
+		H22 = h22;
+	}
+	public Double getH70() {
+		return H70;
+	}
+	public void setH70(Double h70) {
+		H70 = h70;
+	}
+	public Double getH71() {
+		return H71;
+	}
+	public void setH71(Double h71) {
+		H71 = h71;
+	}
+	public Double getH72() {
+		return H72;
+	}
+	public void setH72(Double h72) {
+		H72 = h72;
+	}
+}
